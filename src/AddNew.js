d9c3259386494996b318a88fe9370c439365224b
Lukasz93P <lukasz9.3@interia.pl>
2018-07-15 07:16:37 +0200

    Small changes

--- src/AddNew.js
+++ src/AddNew.js
@@ -4,43 +4,50 @@ import { Field, reduxForm } from 'redux-form'
 import {input} from './login/form/input';
 import Errors from './login/form/errors'
 import {required,minLength2} from './login/form/validators'
+import {Link } from 'react-router-dom';
 
 
 const AddNew = props => {
   
     const {adding, valid,handleSubmit, pristine,errors, submitting} = props
   return (
-    
-    <form onSubmit={handleSubmit(adding)}>
+    <div>
+      <form onSubmit={handleSubmit(adding)}>
 
-          <Field
-            name="name"
-            component="input"
-            type="name"
-            placeholder="name"
-            className="form-control"
-            component={input}
-            label="Name"
-            validate={[required,minLength2]}          />
+            <Field
+              name="name"
+              component="input"
+              type="name"
+              placeholder="name"
+              className="form-control"
+              component={input}
+              label="Name"
+              validate={[required,minLength2]}          />
 
-          <Field
-            name="number"
-            component="input"
-            type="text"
-            placeholder="number"
-            className="form-control"
-            component={input}
-            label="Number"
-            validate={[required]}
-          />
+            <Field
+              name="number"
+              component="input"
+              type="text"
+              placeholder="number"
+              className="form-control"
+              component={input}
+              label="Number"
+              validate={[required]}
+            />
 
-        <button type="submit" className="bg-light-purple dim grow br3" disabled={!valid || pristine || submitting}>
-          Add Contact
+          <button type="submit" className="bg-light-purple dim grow br3" disabled={!valid || pristine || submitting}>
+            Add Contact
+          </button>      
+          <div>
+            <Errors errors={errors}/>
+          </div>
+      </form>
+      <Link to='/list'>
+        <button className="bg-light-purple dim grow br3" >
+          Resign  
         </button>
-        <div>
-          <Errors errors={errors}/>
-        </div>
-    </form>
+      </Link>  
+    </div>
   )
 }
 
--- src/smsapi/messageDiv.js
+++ src/smsapi/messageDiv.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MessageDiv = props => {
-  const {send,setMessage} = props
+  const {send,setMessage,back} = props
   return (
     <div>
         <div className="form-group message-text-wrapper">
@@ -9,6 +9,7 @@ const MessageDiv = props => {
             <textarea  onChange={setMessage} className="form-control message-text-area bg-light-blue dib br3 pa3 ma2 grow dim bw2 shadow-3" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
         <button className="bg-light-purple dim grow br3" onClick={send}>Send message</button>
+        <button className="bg-light-purple dim grow br3" onClick={back}>Resign</button>
     </div>
   )
 }
